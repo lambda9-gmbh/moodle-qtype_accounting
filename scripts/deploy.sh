5228dd12fbf341dc4c4bab2f880d4c4d3751a488
Tobias Melzer <tme@lambda9.de>
2026-02-04 12:00:57 +0100

    better structure for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,17 +13,9 @@ NC='\033[0m' # No Color
 
 echo -e "${YELLOW}Deploying MoFT to ${REMOTE_HOST}:${REMOTE_DIR}${NC}"
 
-# Copy docker folder
-echo "Copying docker folder..."
-scp -r "./docker" "${REMOTE_HOST}:${REMOTE_DIR}/"
-
 # Copy plugin folder
 echo "Copying plugin folder..."
 scp -r "./plugin" "${REMOTE_HOST}:${REMOTE_DIR}/"
-
-# Copy docker-compose.yml
-echo "Copying docker-compose.yml..."
-scp "./docker-compose.yml" "${REMOTE_HOST}:${REMOTE_DIR}/"
 
 # Restart docker compose on remote server
 echo "Restarting Docker Compose on remote server..."
