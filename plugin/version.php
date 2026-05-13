a2d4d2140c94f88ce91d96a4e83c4727d312badf
Tobias Melzer <tme@lambda9.de>
2026-05-13 13:20:10 +0200

    fixed moodle version requirement

--- plugin/version.php
+++ plugin/version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_buchungssatz';
 $plugin->version = 2024010130;
-$plugin->requires = 2020110900; // Moodle 3.10+ (for compatibility testing)
+$plugin->requires = 2022112800; // Moodle 4.1+
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1.0';
