d2cc0ae6f7cf7cb6202ae67b2bdae23989b2c4cd
Tobias Melzer <tme@lambda9.de>
2026-05-13 13:24:26 +0200

    correct license header

--- plugin/amd/src/question.js
+++ plugin/amd/src/question.js
@@ -1,4 +1,4 @@
-// This file is part of MoFT BuSa - Moodle Question Type Buchungssatz.
+// This file is part of Moodle - https://moodle.org/
 //
 // MoFT BuSa is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
--- plugin/lang/de/qtype_buchungssatz.php
+++ plugin/lang/de/qtype_buchungssatz.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of MoFT BuSa - Moodle Question Type Buchungssatz.
+// This file is part of Moodle - https://moodle.org/
 //
 // MoFT BuSa is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
